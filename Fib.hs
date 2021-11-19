602dff8f2ea70e89e6cce25bb91f5c8f4293c24a
ritaramada <anaritaramada@gmail.com>
2021-11-19 16:34:32 +0000

    1.1 done

---
+++ Fib.hs
@@ -0,0 +1,9 @@
+--1.1
+fibRec:: (Integral a) => a -> a
+fibRec 0 = 0
+fibRec 1 = 1
+fibRec n = fibRec(n-1) + fibRec(n-2)
+
+--1.2
+fibLista:: [Integer]
+fibLista = 0 : 1 : zipWith (+) fibLista (tail fibLista)
