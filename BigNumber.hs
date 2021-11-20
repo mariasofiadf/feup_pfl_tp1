6d251b2333a1325262a82237ae77fc043c9c4361
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2021-11-20 20:52:47 +0000

    scanner

---
+++ BigNumber.hs
@@ -0,0 +1,23 @@
+import Data.Char (digitToInt, intToDigit)
+
+data BigNumber = BigNumber Sign [Int] deriving Show
+
+data Sign = Pos | Neg deriving Show
+
+
+--"1234"
+scanner:: String -> BigNumber
+scanner (x:xs) = BigNumber (getSign x) (getDigits (x:xs))
+
+getSign :: Char -> Sign
+getSign s   | s == '-' = Neg
+            | s `elem` ['0','1','2','3','4','5','6','7','8','9'] = Pos
+            | otherwise = error ("Invalid Input")
+ 
+getDigits :: String -> [Int]
+getDigits "" = []
+getDigits (x:[])    | x `elem` ['0','1','2','3','4','5','6','7','8','9'] = [digitToInt x]
+                    | x == '-' = []
+                    | otherwise = error ("Invalid Input")
+getDigits (x:xs) = getDigits [x] ++ getDigits xs
+
