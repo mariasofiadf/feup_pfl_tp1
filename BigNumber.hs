37d4f44d7ba2924477939926954c2bfb299a9213
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2021-11-22 17:10:48 +0000

    output

--- BigNumber.hs
+++ BigNumber.hs
@@ -21,3 +21,13 @@ getDigits (x:[])    | x `elem` ['0','1','2','3','4','5','6','7','8','9'] = [digi
                     | otherwise = error ("Invalid Input")
 getDigits (x:xs) = getDigits [x] ++ getDigits xs
 
+getSignFromBN :: BigNumber -> Sign
+getSignFromBN (BigNumber sign _) = sign
+
+getDigitsFromBN :: BigNumber -> String
+getDigitsFromBN (BigNumber _ []) = ""
+getDigitsFromBN (BigNumber _ (x:xs)) = [intToDigit x] ++ (getDigitsFromBN (BigNumber Pos xs))
+
+
+output :: BigNumber -> String
+output bn = ""
