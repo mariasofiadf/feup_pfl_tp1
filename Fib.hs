85ab15c367a87993d9cf87551e6a46f0adee4b4e
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2021-11-25 16:16:19 +0000

    Fib functions

--- Fib.hs
+++ Fib.hs
@@ -4,8 +4,14 @@ fibRec 0 = 0
 fibRec 1 = 1
 fibRec n = fibRec(n-1) + fibRec(n-2)
 
+fibs :: (Integral a) => [a]
+fibs = [0,1] ++ zipWith (+) fibs (tail fibs)
 --1.2
-fibLista:: [Integer]
-fibLista = [0,1] ++ zipWith (+) fibLista (tail fibLista)
+fibLista :: (Integral a) => a -> a
+fibLista n = fibs !! (fromIntegral n)
+
+--1.3
+fibListaInfinita :: (Integral a) => a -> a
+fibListaInfinita n = list !! (fromIntegral n) where list = [0,1] ++ zipWith (+) list (tail list)
 
 --fibLista:: (Integral a) => a -> a
